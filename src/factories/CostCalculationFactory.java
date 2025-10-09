a14799dc837ae2efdb84451a7342c3d2e41e951a
Parshwa0409 <parshwapatil9@gmail.com>
2025-10-09 17:47:46 +0530

    Updating cost calculation strategy

--- src/factories/CostCalculationFactory.java
+++ src/factories/CostCalculationFactory.java
@@ -9,9 +9,9 @@ import strategy.costCalculation.WithinOneDayCalculation;
 public class CostCalculationFactory {
     public static CostCalculation getCostCalculation(Ticket ticket){
         double hoursParked = ticket.getHoursParked();
-        if(hoursParked <= 1.00){
+        if(hoursParked < 1.00){
             return new UnderOneHourCalculation(ticket);
-        }else if(hoursParked <= 24){
+        }else if(hoursParked < 24){
             return new WithinOneDayCalculation(ticket);
         }else {
             return new OverOneDayCalculation(ticket);
